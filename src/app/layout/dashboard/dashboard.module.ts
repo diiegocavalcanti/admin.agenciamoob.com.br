a4b063d33268c63f7e44dea8bbf04481cf38e501
Cavalcanti <carlos.d.cavalcanti@accenture.com>
2018-06-12 13:39:42 -0300

    Instalação de novos ícones

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -9,11 +9,12 @@ import {
     NotificationComponent,
     ChatComponent
 } from './components';
-import { StatModule } from '../../shared';
+import { StatModule, PageHeaderModule } from '../../shared';
 
 @NgModule({
     imports: [
         CommonModule,
+        PageHeaderModule,
         NgbCarouselModule.forRoot(),
         NgbAlertModule.forRoot(),
         DashboardRoutingModule,
